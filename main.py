fb92c28267ca4675e8bec7407e30f9a22fbb959a
Over-haven <tanushdas26@gmail.com>
2024-10-20 21:07:36 +0530

    Hello

---
+++ main.py
@@ -0,0 +1,15 @@
+#hi
+print("marks obtain in 4 subject :")
+maths = int(input("maths :"))
+science = int(input("science :"))
+english = int(input("english :"))
+sst = int(input("sst :"))
+
+# calculate
+sum = maths+science+english+sst
+print("sum of maths , science , english and sst")
+
+perc = (sum/400)*100
+
+print(end = "percentage mark =")
+print(perc)
